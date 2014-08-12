6dc802e1137187621a817f4c47b0e8f1c1a10083
IPhantom <ivannn787@gmail.com>
2014-08-13 00:33:12 +0300

    added tagFilter filter

---
+++ js/filters/tagFilter.js
@@ -0,0 +1,27 @@
+(function () {
+    'use strict';
+
+    angular.module('stackoverflow').filter('tagFilter', function () {
+        return function (query, tagName) {
+
+            if (!tagName) {
+                return query;
+            }
+
+            var lowerCasedTag = tagName.toLowerCase();
+
+            var result = query.filter(function (element) {
+                var tags = element.tags;
+
+                var passed = tags.some(function (value) {
+                    var lowerCasedValue = value.toLowerCase();
+                    return lowerCasedValue.indexOf(lowerCasedTag) !== -1;
+                });
+
+                return passed;
+            });
+
+            return result;
+        }
+    });
+})();
